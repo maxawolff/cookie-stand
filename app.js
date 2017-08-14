64af3fbbf82ea6773ad3afbe2c39e5bb9e38dd06
maxawolff <maxawolff@hotmail.com>
2017-08-14 14:38:02 -0700

    added three methods to the store 1 object, one for generating  customers per hour, one for the amount of cookies purchased in that hour, and one to make an array of all of the sales for the day

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+var shop1 = {
+  name: '1st & Pike',
+  minCustomers: 23,
+  maxCustomers: 65,
+  cookiesPerSale: 6.3,
+  numCustomers: 0,
+  cookiesSold: 0,
+  hourlySales: [],
+
+  customersPerHour : function(){
+    this.numCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
+    return this.numCustomers;
+  },
+  cookiesPurchased : function(){
+    this.cookiesSold = this.customersPerHour() * this.cookiesPerSale;
+    return this.cookiesSold;
+  },
+
+  calculateSales : function(){
+    for(var i = 0; i < 15; i++){
+      var temp = this.cookiesPurchased();
+      this.hourlySales[i] = temp;
+    }
+    return this.hourlySales;
+  }
+
+};
