7d1bc3fe30251deace9fc31a66997ab0b49cbb86
maxawolff <maxawolff@hotmail.com>
2017-08-15 16:16:28 -0700

    table now holds data for each location at each hour including a daily location total

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var rowOne = ['      ', '6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', 
 var tr = document.createElement('tr');
 tableStart.appendChild(tr);
 
+for(var i = 0; i < rowOne.length; i ++){
+  var td = document.createElement('td');
+  td.innerText = rowOne[i];
+  tableStart.appendChild(td);
+}
+
 var storeHours = ['6AM','7AM','8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6AM', '7AM'];
 
 function Shop(name, minCustomers, maxCustomers, cookiesPerSale){
@@ -30,23 +36,24 @@ function Shop(name, minCustomers, maxCustomers, cookiesPerSale){
   };
 
   this.calculateSales = function(){
-    var location = document.getElementById('sales');
-    var locationName = document.createElement('h2');
-    locationName.innerText = this.name;
-    location.appendChild(locationName);
+    var tr = document.createElement('tr');
+    tableStart.appendChild(tr);
 
-    var list = document.createElement('ul');
-    location.appendChild(list);
-    for(var i = 0; i < storeHours.length; i++){
+    var td = document.createElement('td');
+    td.innerText = this.name;
+    tableStart.appendChild(td);
+
+    for(var i = 0; i < storeHours.length; i ++){
       this.hourlySales[i] = Math.round(this.cookiesPurchased());
-      var listItems = document.createElement('li');
-      listItems.innerText = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';
-      list.appendChild(listItems);
+      var td = document.createElement('td');
+      td.innerText = this.hourlySales[i];
+      tableStart.appendChild(td);
       this.totalSales += this.hourlySales[i];
     }
-    var listItems = document.createElement('li');
-    listItems.innerText = 'Total ' + this.totalSales + ' cookies';
-    list.appendChild(listItems);
+
+    var td = document.createElement('td');
+    td.innerText = this.totalSales;
+    tableStart.appendChild(td);
   };
 }
 
@@ -55,14 +62,8 @@ var seatacShop = new Shop('SeaTac Airport', 3, 24, 1.2);
 var seattleCenterShop = new Shop('Seattle Center', 11, 38, 3.7);
 var capitolShop = new Shop('Capitol Hill', 20, 38, 2.3);
 var alkiShop = new Shop('Alki', 2, 16, 4.6);
-// pikeShop.calculateSales();
-// seatacShop.calculateSales();
-// seattleCenterShop.calculateSales();
-// capitolShop.calculateSales();
-// alkiShop.calculateSales();
-
-for(var i = 0; i < rowOne.length; i ++){
-  var td = document.createElement('td');
-  td.innerText = rowOne[i];
-  tableStart.appendChild(td);
-}
+pikeShop.calculateSales();
+seatacShop.calculateSales();
+seattleCenterShop.calculateSales();
+capitolShop.calculateSales();
+alkiShop.calculateSales();
