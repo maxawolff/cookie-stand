a3d116365b2ed10230353f7dd052e1167b7a070f
maxawolff <maxawolff@hotmail.com>
2017-08-21 06:35:42 -0700

    wrote readme

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ var tableStart = document.getElementById('sales');
 var tab = document.createElement('table');
 tableStart.appendChild(tab);
 
-// added a function to create a table row since I saw this code being repeated multiple times
 var addTableRow = function(){
   var tr = document.createElement('tr');
   tableStart.appendChild(tr);
 };
 
-// added a function to create table data for the same reason as above.
 var addTableData = function(data, cName){
   var td = document.createElement('td');
   td.innerText = data;
@@ -22,7 +20,6 @@ var addTableData = function(data, cName){
   tableStart.appendChild(td);
 };
 
-//function to make the header rom of the table
 var makeHeader = function(){
   addTableRow();
   for(var i = 0; i < rowOne.length; i ++){
@@ -30,7 +27,6 @@ var makeHeader = function(){
   }
 };
 
-//constructor function for the shop items
 function Shop(name, minCustomers, maxCustomers, cookiesPerSale){
   this.name = name;
   this.minCustomers = minCustomers;
@@ -64,7 +60,6 @@ function Shop(name, minCustomers, maxCustomers, cookiesPerSale){
   };
 }
 
-//function to make the footer row for the table
 var makeFoot = function(){
   addTableRow();
   addTableData('totals','footer');
@@ -83,7 +78,6 @@ var makeFoot = function(){
   addTableData(finalTotal,'footer');
 };
 
-// create all of the shop objects
 var pikeShop = new Shop('1st and Pike', 23, 65, 6.3);
 var seatacShop = new Shop('SeaTac Airport', 3, 24, 1.2);
 var seattleCenterShop = new Shop('Seattle Center', 11, 38, 3.7);
@@ -91,7 +85,6 @@ var capitolShop = new Shop('Capitol Hill', 20, 38, 2.3);
 var alkiShop = new Shop('Alki', 2, 16, 4.6);
 var shops = [pikeShop, seatacShop, seattleCenterShop, capitolShop, alkiShop];
 
-//calls functions to actually run code!
 makeHeader();
 pikeShop.render();
 seatacShop.render();
@@ -100,7 +93,6 @@ capitolShop.render();
 alkiShop.render();
 
 var form = document.getElementById('theForm');
-// var submit = document.getElementById('submit');
 
 function harvestAndPost(event){
   event.preventDefault();
